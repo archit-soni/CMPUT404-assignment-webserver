da6a99bba37b5b6444a00b8babc87f114a98eae2
Archit <archit2@ualberta.ca>
2022-01-28 18:25:32 +0530

    server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +29,92 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
+
         print ("Got a request of: %s\n" % self.data)
+        self.data = self.data.decode("utf-8")
+        self.data = self.data.splitlines()
+        if self.data[0][:3] != "GET":
+            self.notGet()
+        else:
+            self.get(self.data)
         self.request.sendall(bytearray("OK",'utf-8'))
+
+    def notGet(self):
+        self.response = "HTTP/1.1 405 Method Not Allowed\r\n"
+        self.request.send(bytearray(self.response,'utf-8'))
+        return
+
+    def get(self, data):
+        self.notRoot(data[0][4:7])
+        if self.data[0].split(" ")[1][-1]=="/":
+            self.fetchIndex(self.data[0].split(" ")[1])
+        elif self.data[0].split(" ")[1][-4:]==".css":
+            self.getCSS(self.data[0].split(" ")[1])
+        elif self.data[0].split(" ")[1][-5:]==".html":
+            self.getHTML(self.data[0].split(" ")[1])
+        else:
+            self.redirect(self.data[0].split(" ")[1])
+
+    def getHTML(self, url):
+        try:
+            file = open("./www"+url)
+            f = file.read()
+            file.close()
+        except:
+            self.response = "HTTP/1.1 404 Not Found\r\n"
+            self.request.send(bytearray(self.response, 'utf-8'))
+            return
+        self.response = f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(f)}\r\n\r\n{f}'
+        self.request.send(bytearray(self.response, 'utf-8'))
+        return
+
+    def fetchIndex(self, url):
+        try:
+            file = open("./www"+url+"index.html")
+            f = file.read()
+            file.close()
+        except:
+            self.response = "HTTP/1.1 404 Not Found\r\n"
+            self.request.send(bytearray(self.response, 'utf-8'))
+            return
+        self.response = f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(f)}\r\n\r\n{f}'
+        self.request.send(bytearray(self.response, 'utf-8'))
+        return
+            
+    def redirect(self, url):
+        try:
+            file = open("./www"+url+"/index.html")
+            f = file.read()
+            file.close()
+        except:
+            self.response = "HTTP/1.1 404 Not Found\r\n"
+            self.request.send(bytearray(self.response, 'utf-8'))
+            return
+        self.response = f'HTTP/1.1 301 Moved Permanently\r\nLocation: {url}/\r\n'
+        self.request.send(bytearray(self.response, 'utf-8'))
+        return
+
+    def notRoot(self, url):
+        if url == "/..":
+            self.response = "HTTP/1.1 404 Not Found\r\n"
+            self.request.send(bytearray(self.response, 'utf-8'))
+            return
+
+    def getCSS(self, url):
+        try:
+            file = open("./www"+url)
+            f = file.read()
+            file.close()
+        except:
+            self.response = "HTTP/1.1 404 Not Found\r\n"
+            self.request.send(bytearray(self.response, 'utf-8'))
+            return
+        self.response = f'HTTP/1.1 200 OK\r\nContent-Type: text/css\r\nContent-Length: {len(f)}\r\n\r\n{f}'
+        self.request.send(bytearray(self.response, 'utf-8'))
+        return
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
